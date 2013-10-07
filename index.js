9565c05aaac63609fea1aa8be2482a1b46bbdae6
jheytompkins <tleith@Toms-MacBook-Pro.local>
2013-10-07 17:10:15 +0100

    Created scaffolding of formation using create script.

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = formation;
+
+function formation(element, options) {
+	if (!(this instanceof formation)) return new formation(element, options);
+	this.defaults = {};
+	this.element = element;
+	this.options = options;
+}
+formation.prototype.create = function () {
+
+}
+formation.prototype.init = function () {
+
+}
+formation.prototype._move = function () {
+
+}
+formation.prototype.goForward = function () {
+
+}
+formation.prototype.goBackward = function () {
+
+}
+formation.prototype.goToIndex = function () {
+
+}
+formation.prototype._bindNav = function () {
+
+}
+formation.prototype._createNav = function () {
+
+}
+formation.prototype.val = function () {
+
+}
